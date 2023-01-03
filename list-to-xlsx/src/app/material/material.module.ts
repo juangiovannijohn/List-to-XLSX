a059b37f85d53d59838efb0f8765ff38a0c9a34b
Juan Manuel <rodriguezjuanm@live.com>
2023-01-03 19:10:59 -0300

    feat(material modules): se dejan de usar componentes de material

--- list-to-xlsx/src/app/material/material.module.ts
+++ list-to-xlsx/src/app/material/material.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { MatTableModule } from '@angular/material/table'
 import {MatIconModule} from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 
@@ -18,8 +16,6 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
   exports:[
     MatTableModule,
     MatIconModule,
-    MatButtonModule,
-    MatFormFieldModule,
     MatInputModule,
     MatSnackBarModule
   ]
